cd73483fec51c2af38b961fc7dd90393741e2f4f
toxicbishop <pranavarun19@gmail.com>
2026-06-17 19:47:45 +0530

    fix: clear Vite cache conflict, remove duplicate CSS reset, clean up main.tsx imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './app.scss'
 import App from './App'
 
 const rootElement = document.getElementById('root')
